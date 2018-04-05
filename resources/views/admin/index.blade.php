4404e5439edbc02fd9f767ac8613fd6fc235a766
koxxxa3227@yandex.ru <dump3227>
2018-04-05 22:14:43 +0300

    Added quick access to admin's functionality

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,7 +1,17 @@
 @extends("layouts.app")
 
 @section("content")
-    <div class="container">
-
+    <div class="container my-2">
+        <div class="row">
+            <div class="col-md-4">
+                <a href="{{action('Admin\PageController@users')}}" class="btn btn-primary btn-block">Пользователи</a>
+            </div>
+            <div class="col-md-4">
+                <a href="{{action('Admin\PageController@payments')}}" class="btn btn-primary btn-block">Выплаты</a>
+            </div>
+            <div class="col-md-4">
+                <a href="{{action('Admin\PageController@deposits')}}" class="btn btn-primary btn-block">Депозиты</a>
+            </div>
+        </div>
     </div>
 @endsection
